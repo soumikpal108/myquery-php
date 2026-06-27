f6c374fd7dd9b509d33094fb31bf21e6e07d0c87
soumikpal108 <soumikpal797@gmail.com>
2026-06-27 13:18:31 +0530

    Add responsive navigation bar

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MyQuery Project</title>
+    <?php include('./client/commonFiles.php') ?>
+</head>
+<body>
+    <?php include('./client/header.php') ?>
+</body>
+</html>
